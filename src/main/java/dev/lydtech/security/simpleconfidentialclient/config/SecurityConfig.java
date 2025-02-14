130f9aae17474b8d867a64721090db2665979824
ck.lee <ck.lee@mimos.my>
2025-02-14 14:02:41 +0800

    move package

--- src/main/java/dev/lydtech/security/simpleconfidentialclient/config/SecurityConfig.java
+++ src/main/java/dev/lydtech/security/simpleconfidentialclient/config/SecurityConfig.java
@@ -1,24 +1,40 @@
-package dev.lydtech.security.simpleconfidentialclient;
+package dev.lydtech.security.simpleconfidentialclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import dev.lydtech.security.simpleconfidentialclient.LoggingRequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,6 +42,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 class SecurityConfig {
 
     @Bean
@@ -42,7 +59,7 @@ class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler, OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> tokenResponseClient) throws Exception {
         http.authorizeHttpRequests(authorise ->
                 authorise
                         .requestMatchers("/")
@@ -50,15 +67,34 @@ class SecurityConfig {
                         .requestMatchers("/admin*")
                         .hasRole("admin")
                         .requestMatchers("/users*")
-                        .hasAnyRole("user", "admin")
+                        .hasAnyRole("user", "admin", "USER", "ADMIN")
                         .requestMatchers("/tokens*")
-                        .hasAnyRole("user", "admin")
+                        .hasAnyRole("user", "admin", "USER", "ADMIN")
                         .anyRequest()
                         .authenticated());
         http.oauth2Login(withDefaults())
+        // http.oauth2Login(oauth2 -> oauth2.tokenEndpoint(token -> token.accessTokenResponseClient(accessTokenResponseClient())))
                 .logout(logout ->
                         logout.logoutSuccessHandler(oidcLogoutSuccessHandler));
         return http.build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new LoggingRequestInterceptor());
+        restTemplate.setInterceptors(interceptors);
+
+        return restTemplate;
+    }
+
+    @Bean
+    public DefaultAuthorizationCodeTokenResponseClient accessTokenResponseClient() {
+        DefaultAuthorizationCodeTokenResponseClient client = new DefaultAuthorizationCodeTokenResponseClient();
+        client.setRestOperations(restTemplate()); // ✅ Use custom RestTemplate
+        return client;
     }
 
     @Bean
@@ -73,14 +109,23 @@ class SecurityConfig {
 
                     // Map the claims found in idToken and/or userInfo
                     // to one or more GrantedAuthority's and add it to mappedAuthorities
+                    // NOTES: MyOne SSO does not store realm_access under User Info
                     Map<String, Object> realmAccess = userInfo.getClaim("realm_access");
                     Collection<String> realmRoles;
                     if (realmAccess != null
                             && (realmRoles = (Collection<String>) realmAccess.get("roles")) != null) {
                         realmRoles
                                 .forEach(role -> mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+                            mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
                     }
 
+                }
+
+                
+                if (authority instanceof SimpleGrantedAuthority simpleGrantedAuthority) {
+                    log.info("Role: " + simpleGrantedAuthority.getAuthority());
+                    // FIXME: Temporary add ROLE_USER to any authenticated users
+                    mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
                 }
             });
 
